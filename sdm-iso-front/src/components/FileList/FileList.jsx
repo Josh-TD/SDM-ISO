41f152724c097c51049bca5753c95c9adf7d4d89
Tim <persontim11@gmail.com>
2023-10-09 15:02:05 -0400

    added top bar

--- sdm-iso-front/src/components/FileList/FileList.jsx
+++ sdm-iso-front/src/components/FileList/FileList.jsx
@@ -2,17 +2,17 @@ import React from "react";
 
 export default function FileList() {
   return (
-    <div className="grid grid-cols-[15%,85%] grid-rows-[5%,95%] h-full">
-      {/* Left 20% for filters */}
+    <div className="grid grid-cols-[15%,85%] grid-rows-[7%,93%] h-full">
+      {/* Filters */}
       <div className="col-start-1 row-span-2 pr-1">
         <div className="bg-white container w-full flex flex-col shadow-[10px_0px_8px_-8px_#a0aec0]">
 
           {/* header */}
-          <div className="text-base text-iso-secondary-text pl-4 pt-1 pb-16">Filtered by:</div>
+          <div className="text-base font-semibold text-iso-secondary-text pl-4 pt-1 pb-16">Filtered by:</div>
 
           {/* Filter item */}
           <div className="flex items-center justify-between border-y-4 border-iso-border-light">
-            <div className="grow text-base text-black pl-4 pr-6 my-4 cursor-pointer">FileType</div>
+            <div className="grow text-base font-semibold text-iso-secondary-text pl-4 pr-6 my-4 cursor-pointer">FileType</div>
             <svg 
               className="w-6 h-6 mr-3 cursor-pointer"
               xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
@@ -22,7 +22,7 @@ export default function FileList() {
 
           {/* Filter item */}
           <div className="flex items-center justify-between border-y-4 border-iso-border-light">
-            <div className="grow text-base text-black pl-4 pr-6 my-4 cursor-pointer">Project</div>
+            <div className="grow text-base font-semibold text-iso-secondary-text pl-4 pr-6 my-4 cursor-pointer">Project</div>
             <svg 
               className="w-6 h-6 mr-3 cursor-pointer"
               xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
@@ -33,12 +33,38 @@ export default function FileList() {
         </div>
       </div>
 
-      {/* Top 5% for various functions */}
-      <div className="bg-blue-500 col-start-2 row-start-1 p-4">
-        {/* Sort, Select Multiple, Download/View */}
+      {/* Top Bar */}
+      <div className="bg-white col-start-2 row-start-1 flex items-center justify-start">
+
+        {/* Top bar item */}
+        {/* <div className="flex items-center justify-between mx-3">
+          <div className="text-base font-semibold text-iso-secondary-text">Sort:&nbsp;</div>
+          <div className="text-base font-semibold text-iso-secondary-text cursor-pointer">Default</div>
+          <svg 
+            className="w-5 h-5 cursor-pointer pt-1"
+            xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+            <path fillRule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clipRule="evenodd" />
+          </svg>
+        </div> */}
+
+        {/* Top bar item */}
+        <div className="inline-flex items-center justify-between mx-3">
+          <div className="pr-1">
+            <input type="checkbox" id="selectMultiple" name="selectMultiple" value="selectMultiple"></input>
+          </div>
+          <label for="html" className="text-base font-semibold text-iso-secondary-text">Select Multiple</label>
+        </div>
+
+        {/* Top bar item */}
+        <div className="flex items-center justify-between mx-3">
+          <div className="text-base font-semibold text-iso-secondary-text cursor-pointer">Download</div>
+          <div className="text-base font-semibold text-iso-secondary-text">&nbsp;|&nbsp;</div>
+          <div className="text-base font-semibold text-iso-secondary-text cursor-pointer">View</div>
+        </div>
+
       </div>
 
-      {/* Right 80% for file list */}
+      {/* File List */}
       <div className="bg-iso-slate col-start-2 row-start-2 p-4">
         {/* File list */}
       </div>
