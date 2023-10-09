4e89bcb6b1d523219e1e93f56010df95fb6cea2f
Tim <persontim11@gmail.com>
2023-10-08 21:57:36 -0400

    adjusted file list layout display

--- sdm-iso-front/src/components/FileList/FileList.jsx
+++ sdm-iso-front/src/components/FileList/FileList.jsx
@@ -24,20 +24,20 @@ export default function FileList() {
 
 export function FileListLayout() {
   return (
-    <div className="grid grid-cols-[12%,88%] grid-rows-[5%,95%]  h-full">
+    <div className="grid grid-cols-[12%,88%] grid-rows-[7%,93%]  h-full">
       {/* Left 12% for filters */}
-      <div className="bg-gray-200 col-start-1 row-span-2 p-4">
-        {/* Filters */}
+      <div className="bg-gray-200 col-start-1 row-span-2 p-3">
+        <h5>Filters</h5>
       </div>
 
       {/* Top 5% for various functions */}
-      <div className="bg-blue-500 col-start-2 row-start-1 p-4">
-        {/* Sort, Select Multiple, Download/View */}
+      <div className="bg-blue-500 col-start-2 row-start-1 p-1">
+        <h5>Top Bar</h5>
       </div>
 
       {/* Right 88% for file list */}
       <div className="bg-iso-slate col-start-2 row-start-2 p-4">
-        {/* File list */}
+        <h5>File List</h5>
       </div>
     </div>
   );
