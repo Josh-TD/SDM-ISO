90b2934e35138cdef0ec4f410dab0cb66f608812
Michaela DeToma <mdetoma@umass.edu>
2023-10-09 21:14:40 -0400

    added box to login screen

--- sdm-iso-front/src/components/AuthBox/AuthBox.jsx
+++ sdm-iso-front/src/components/AuthBox/AuthBox.jsx
@@ -2,28 +2,29 @@ import React from 'react';
 
 export function AuthBox() {
   return (
-    <div className="w-full flex items-center justify-center ">
+    <div class="min-w-screen min-h-0 h-full flex justify-center items-center">
+    <div class="relative bg-iso-offwhite w-2/5 h-2/3 shadow">
+        <span class="m-10 flex justify-center items-center font-semibold text-iso-dark-text">Please enter your email address and password.</span>
+        <div className="w-full flex items-center justify-center ">
 
-      {/* Email Address*/}
-      <div>
-        <label>Email Address:</label>
-        <input>
-
-
-        </input>
-      </div>
-
-      {/* Password*/}
-      <div>
-        <label>Password:</label>
-          <input>
-
-
-          </input>
-        
+          {/* Email Address*/}
+          <div>
+            <label>Email Address:</label>
+            <input>
+            </input>
+          </div>
+    
+          {/* Password*/}
+          <div>
+            <label>Password:</label>
+            <input>
+            </input>
+          </div>
+            
+        </div>
       </div>
     </div>
-  );
-};
+
+  )};
 
 export default AuthBox;
