353f8557bbdb125019bdd669d92f37d3d83b0a70
Michaela DeToma <mdetoma@umass.edu>
2023-10-10 17:07:18 -0400

    added forgot password

--- sdm-iso-front/src/components/AuthBox/AuthBox.jsx
+++ sdm-iso-front/src/components/AuthBox/AuthBox.jsx
@@ -5,36 +5,38 @@ export function AuthBox() {
     <div class="min-w-screen min-h-0 h-full flex justify-center items-center">
       <div class="relative bg-iso-offwhite w-2/5 h-2/3 shadow">
         <span class="m-10 flex justify-center items-center font-semibold text-iso-dark-text">Please enter your email address and password.</span>
-        <div className="w-full display-block">
+        <div className="w-full display-block items-center">
           {/* Email Address*/}
-          <div class="display-block mb-5 ">
-            <label class="m-2 top-0 font-semibold text-iso-light-gray" for="email">Email Address:</label>
-            <input class="shadow cursor-text">
+          <div class="mb-5">
+            <label class="m-2 top-0 font-semibold text-iso-light-gray justify-right" for="email">Email Address:</label>
+            <input class="shadow cursor-text h-7">
             </input>
           </div>
 
           {/* Password*/}
-          <div class="display-block">
-            <label class="m-2 pr-4 font-semibold text-iso-light-gray" for="password">Password:</label>
-            <input class="pl-4 shadow cursor-text">
+          <div class="justify-center">
+            <label class="m-2 pr-4 font-semibold text-iso-light-gray justify-right" for="password">Password:</label>
+            <input class="pl-4 shadow cursor-text h-7">
             </input>
           </div>
 
           {/* container for the inline Keep Me Signed In and Sign In button */}
-          <div class="display-block w-full min-h-">
-            <div className="inline-flex items-center justify-between mx-3 float-left">
+          <div class="display-block w-full m-5">
+            <div className="inline-flex items-center justify-between mx-3">
               <div className="pr-1">
                 <input class="cursor-pointer" type="checkbox" id="staySignedIn" name="staySignedIn" value="staySignedIn"></input>
               </div>
-              <label for="html" className="text-base font-semibold text-iso-secondary-text">Keep Me Signed In</label>
+              <label for="html" className="text-base font-semibold text-iso-light-gray">Keep Me Signed In</label>
             </div>
 
-            <div class="display-inline-block float-right">
-              <div class="display-block">
-                <button class="bg-iso-medium-gray hover:bg-iso-link-blue text-white font-bold py-2 px-4 rounded cursor-pointer">Sign In</button>
+              <div class = "inline-flex m-5">
+                <button class="bg-iso-medium-gray hover:bg-iso-link-blue text-white font-bold py-2 px-4 rounded cursor-pointer float-right">Sign In</button>
               </div>
-            </div>
-          </div>  
+          </div> 
+
+          <div class="w-full items-right">
+            <a class="text-iso-link-blue">Forgot your password?</a>
+          </div> 
  
         </div>
       </div>
