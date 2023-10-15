60d783c8fd330e4d301a1ab8204dac18fa6f6aec
Niya Shroff <nshroff@umass.edu>
2023-10-15 11:24:18 -0400

    Backend packaging

---
+++ sdm-iso-back/src/main/java/com/example/sdmisoback/BackendServerApp.java
@@ -0,0 +1,13 @@
+package com.example.sdmisoback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendServerApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendServerApp.class, args);
+    }
+
+}
---
+++ sdm-iso-back/src/test/java/com/example/sdmisoback/BackendServerAppTests.java
@@ -0,0 +1,13 @@
+package com.example.sdmisoback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendServerAppTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
