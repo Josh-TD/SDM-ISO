11bf745820abc0ddbf9d964c2db8bbf84b585758
Niya Shroff <nshroff@umass.edu>
2023-10-15 14:45:00 -0400

    SDM12 WIP: Testing frontend connection

---
+++ sdm-iso-back/src/main/java/com/example/sdmisoback/repository/UsersRepository.java
@@ -0,0 +1,7 @@
+package com.example.sdmisoback.repository;
+
+import com.example.sdmisoback.model.Users;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UsersRepository extends CrudRepository<Users, Long> {
+}
