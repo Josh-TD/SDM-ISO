85d7a85002b4b89c22645fc0a0d5aa36f607cca2
Michaela DeToma <mdetoma@umass.edu>
2023-10-19 18:07:42 -0400

    added RecentlyViewed.java

---
+++ sdm-iso-back/src/main/java/com/example/sdmisoback/model/RecentlyViewed.java
@@ -0,0 +1,38 @@
+package com.example.sdmisoback.model;
+import java.util.Date;
+
+public class RecentlyViewed {
+    private AttachmentFile file;
+    private Date timestamp;
+
+
+    //constructors
+    public RecentlyViewed(){
+    }
+
+    public RecentlyViewed(AttachmentFile file, Date timestamp){
+        this.file = file;
+        this.timestamp = timestamp;
+    }
+
+    //getters and setters
+
+    public AttachmentFile getFile(){
+        return this.file;
+    }
+
+    public Date getTimestamp(){
+        return this.timestamp;
+    }
+
+    public void setFile(AttachmentFile file){
+        this.file = file;
+    }
+
+    public void setTimestamp(Date timestamp){
+        this.timestamp = timestamp;
+    }
+
+}
+
+
