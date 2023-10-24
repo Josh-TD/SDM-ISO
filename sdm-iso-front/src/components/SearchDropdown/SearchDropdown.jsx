96ae5dd88ae7619294bc0899209d9532af3d1a95
Michaela DeToma <mdetoma@umass.edu>
2023-10-24 13:31:38 -0400

    Added search by dropdown with options

---
+++ sdm-iso-front/src/components/SearchDropdown/SearchDropdown.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function SearchDropdown() {
+    return (
+        <div class="flex items-center w-49 space-x-2">
+          <label for="search-by" class="text-sm font-light text-gray-600 whitespace-nowrap">Search by</label>
+          <select id="search-by" name="search-by" class="block w-full mt-1 py-2 px-3 border focus-ring bg-white focus:border-blue-300">
+            <option value="select">Select...</option>
+            <option value="file-name">File Name</option>
+            <option value="customer-name">Customer Name</option>
+            <option value="proposal-name">Proposal Name</option>
+            <option value="resource-name">Resource Name</option>
+          </select>
+        </div>
+    )
+}
+
