9da72b3039bf39d22d0f3f1abedf0655b58d7c47
Michaela DeToma <mdetoma@umass.edu>
2023-10-25 11:30:28 -0400

    Added search dropdown

--- sdm-iso-front/src/components/PageTitle/PageTitle.jsx
+++ sdm-iso-front/src/components/PageTitle/PageTitle.jsx
@@ -16,6 +16,7 @@ export function PageTitleWithSearchBar() {
     <div className="bg-iso-offwhite w-full">
       <div className="container pl-10 flex items-center justify-between w-full font-light text-iso-slate">
         <div className="grow py-4 text-5xl">SDM File Manager</div>
+        <SearchDropdown />
         <SearchBar />
       </div> 
     </div> 
