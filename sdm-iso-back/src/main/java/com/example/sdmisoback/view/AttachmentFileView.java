3558466c02ba1943b7a4fa4a806b3b395339dfd2
Tim <persontim11@gmail.com>
2023-11-08 13:55:48 -0500

    updated comment and removed old view

--- sdm-iso-back/src/main/java/com/example/sdmisoback/view/AttachmentFileView.java
+++ sdm-iso-back/src/main/java/com/example/sdmisoback/view/AttachmentFileView.java
@@ -5,6 +5,9 @@ import java.time.LocalDateTime;
 import com.blazebit.persistence.view.*;
 import com.example.sdmisoback.model.*;
 
+// Acts like a Data-transfer-object(DTO) so that we only send the 
+// most necessary information over to the front end
+// class is temporary and the real FileDTO will include more info
 @EntityView(AttachmentFile.class)
 public interface AttachmentFileView {
 
