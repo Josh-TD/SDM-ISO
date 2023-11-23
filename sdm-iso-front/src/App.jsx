8f34eb7244b99e8d98cbce614c245bd621e0e921
Noam Gans <noamgans@Noams-MacBook-Air.local>
2023-11-23 14:36:08 -0500

    clerk login

--- sdm-iso-front/src/App.jsx
+++ sdm-iso-front/src/App.jsx
@@ -1,31 +1,6 @@
-import { React, useEffect, useState } from 'react';
-import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom'
-import MainPage from './components/MainPage/MainPage'
-import LoginPage from './components/LoginPage/LoginPage'
-import './index.css';
-
-function App() {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-  const [rootPage, setRootPage] = useState(undefined);
-
-  useEffect(() => {
-    const authenticate = () => { setIsAuthenticated(!isAuthenticated) };
-
-    if (isAuthenticated) {
-      setRootPage(<MainPage />)
-    } else {
-      setRootPage(<LoginPage authenticateFn={authenticate} />)
-    }
-  }, [isAuthenticated])
-
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={rootPage} />
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
-    </BrowserRouter>
-  )
+function App(){
+  return(
+    <a href='/sign-in'>Sign In</a>
+  );
 }
-
 export default App
