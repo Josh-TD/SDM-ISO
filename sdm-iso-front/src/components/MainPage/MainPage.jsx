21cec024bbc1d9b48dfca743ca824608f44d8573
Michaela DeToma <mdetoma@umass.edu>
2023-11-26 13:05:30 -0500

    Added beginnings of  advanced search popup

--- sdm-iso-front/src/components/MainPage/MainPage.jsx
+++ sdm-iso-front/src/components/MainPage/MainPage.jsx
@@ -2,15 +2,22 @@ import React from 'react';
 import Header from '../Header/Header';
 import { PageTitleWithSearchBar } from '../PageTitle/PageTitle';
 import FileList from '../FileList/FileList';
+import {useState} from 'react';
+import Popup from '../Misc/Popup';
 
 export default function MainPage() {
+  const [buttonPopup, setButtonPopup] = useState(false);
   return (
     <div className='flex flex-col h-screen'>
       <Header />
       <PageTitleWithSearchBar />
       <main className="flex-grow overflow-visible">
+        <button onClick = {() => setButtonPopup(true)} >Advanced Search</button>
         <FileList />
       </main>
+      <Popup trigger = {buttonPopup} setTrigger = {setButtonPopup}>
+        <h1>Advanced Search</h1>
+      </Popup>
     </div>
   )
 };
