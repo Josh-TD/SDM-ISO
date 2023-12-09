efa470697865a27eca2870860b368b897c8f1df2
Michaela DeToma <mdetoma@umass.edu>
2023-12-09 13:33:08 -0500

    search functionality

--- sdm-iso-front/src/components/RegularSearch/SearchBar/SearchBar.jsx
+++ sdm-iso-front/src/components/RegularSearch/SearchBar/SearchBar.jsx
@@ -1,6 +1,30 @@
-import React from 'react';
+import React, { useState } from "react";
 
-export default function SearchBar(onSearchPressed) {
+export default function SearchBar({onSearchPressed, setSelectedParam}) {
+  const [input, setInput] = useState('');
+
+  const constructEndpoint = (selectedParam, input) => {
+    let parameterURL = ''
+    if (selectedParam.equals("customer-name")){
+      parameterURL = parameterURL + `${"&customerName=" + input}`
+    }
+    if (selectedParam.equals("proposal-name")){
+      parameterURL = parameterURL + `${"&proposalName=" + input}`
+    }
+    if (selectedParam.equals("project-name")){
+      parameterURL = parameterURL + `${"&projectName=" + input}`
+    }
+    if (selectedParam.equals("file-description")){
+      parameterURL = parameterURL + `${"&fileDescription=" + input}`
+    }
+    onSearchPressed(parameterURL)
+};
+
+  const performSearch = () => {
+    constructEndpoint(selectedParam, input);
+    onClosePopup();
+  }
+
   return(
     <div className='mr-4 flex items-center justify-between bg-white border-2 border-iso-border-light'>
       <input
@@ -8,9 +32,12 @@ export default function SearchBar(onSearchPressed) {
         type="text"
         aria-label="search input form"
         placeholder="SEARCH"
+        onChange={(e) => setInput(e.target.value)}
       />
       <svg 
         className="w-6 h-6 mr-4 cursor-pointer"
+        aria-label="search submit button"
+        onClick={performSearch}
         xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
         <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" 
       />
--- sdm-iso-front/src/components/RegularSearch/SearchDropdown/SearchDropdown.jsx
+++ sdm-iso-front/src/components/RegularSearch/SearchDropdown/SearchDropdown.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
 
-export default function SearchDropdown() {
+export default function SearchDropdown(setSelectedParam) {
+  const handleSelectChange = (e) => {
+    setSelectedParam(e.target.value);
+  };
     return (
         <div className="flex items-center w-49 space-x-2">
           <label htmlFor="search-by" className="text-sm font-light text-gray-600 whitespace-nowrap">Search by</label>
-          <select id="search-by" name="search-by" className="block w-full mt-1 py-2 px-3 border focus-ring bg-white focus:border-blue-300">
+          <select 
+          id="search-by" 
+          name="search-by" 
+          onChange={handleSelectChange}
+          className="block w-full mt-1 py-2 px-3 border focus-ring bg-white focus:border-blue-300">
             <option value="select">Select...</option>
             <option value="customer-name">Customer Name</option>
             <option value="proposal-name">Proposal Name</option>
-            <option value="resource-name">Resource Name</option>
-            <option value="resource-name">File Description</option>
+            <option value="project-name">Project Name</option>
+            <option value="file-description">File Description</option>
           </select>
         </div>
     )
