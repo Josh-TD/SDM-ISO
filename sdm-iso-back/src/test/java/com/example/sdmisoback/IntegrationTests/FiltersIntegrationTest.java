8b54ae0b97f241223ceb97778299e056415b52fd
Tim <persontim11@gmail.com>
2023-12-11 01:15:41 -0500

    attachment file tests

--- sdm-iso-back/src/test/java/com/example/sdmisoback/IntegrationTests/FiltersIntegrationTest.java
+++ sdm-iso-back/src/test/java/com/example/sdmisoback/IntegrationTests/FiltersIntegrationTest.java
@@ -29,6 +29,7 @@ public class FiltersIntegrationTest {
 
     private static String baseUrl = "/api/v3/files/list";
     private static String defaultReqParams = "?pageNum=0&pageSize=10&sortBy=createDate&sortAsc=false";
+    private static String defaultRequest = baseUrl + defaultReqParams;
 
     @Test
     public void Filters_NoFilters_ReturnsOkAndCorrectFirstValue() throws Exception {
@@ -46,30 +47,69 @@ public class FiltersIntegrationTest {
     }
 
     @Test 
-    public void Filters_Error403_BadFileTypeSingular() throws Exception{
-        // customer instead of customerName
+    public void Filters_Error403_BadFileTypeSingular() throws Exception {
         mvc.perform(get(baseUrl + defaultReqParams + "&fileTypes=jpeg"))
             .andExpect(status().isBadRequest());
     }
 
     @Test 
-    public void Filters_Error403_BadFileTypeMultiple() throws Exception{
-        // customer instead of customerName
+    public void Filters_Error403_BadFileTypeMultiple() throws Exception {
         mvc.perform(get(baseUrl + defaultReqParams + "&fileTypes=pdf&fileTypes=jpg&fileTypes=jpeg"))
             .andExpect(status().isBadRequest());
     }
 
     @Test 
-    public void Filters_Error403_BadSortBy() throws Exception{
-        // customer instead of customerName
+    public void Filters_Error403_BadSortBy() throws Exception {
         mvc.perform(get(baseUrl + "?pageNum=0&pageSize=10&sortBy=description&sortAsc=false"))
             .andExpect(status().isBadRequest());
     }
 
     @Test 
-    public void Filters_Error404_NoMatch() throws Exception{
-        // customer instead of customerName
+    public void Filters_Error404_NoMatch() throws Exception {
         mvc.perform(get(baseUrl + defaultReqParams + "&fileId=999999"))
             .andExpect(status().isNotFound());
     }
+
+    @Test
+    public void Filters_SingularFilter_FileId() throws Exception {
+        mvc.perform(get(defaultRequest + "&fileId=1001"))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+            .andExpect(jsonPath("$.empty").value(false))
+            .andExpect(jsonPath("$.totalElements").value(1))
+            .andExpect(jsonPath("$.totalPages").value(1))
+            .andExpect(jsonPath("$.content[0].attachmentId").value(1001));
+    }
+
+    @Test
+    public void Filters_SingularFilter_FileIdDuplicates() throws Exception {
+        mvc.perform(get(defaultRequest + "&fileId=1052"))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+            .andExpect(jsonPath("$.empty").value(false))
+            .andExpect(jsonPath("$.totalElements").value(8))
+            .andExpect(jsonPath("$.content[0].attachmentId").value(1052));
+    }
+
+    @Test
+    public void Filters_SingularFilter_FileDescription() throws Exception {
+        mvc.perform(get(defaultRequest + "&fileDescription=resource"))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+            .andExpect(jsonPath("$.empty").value(false))
+            .andExpect(jsonPath("$.totalElements").value(18))
+            .andExpect(jsonPath("$.content[0].attachmentId").value(1013));
+    }
+
+    @Test
+    public void Filters_SingularFilter_FileCreateDate() throws Exception {
+        mvc.perform(get(defaultRequest + "&createdSince=2015-03-08T00:00:00"))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+            .andExpect(jsonPath("$.empty").value(false))
+            .andExpect(jsonPath("$.totalElements").value(40))
+            .andExpect(jsonPath("$.content[0].attachmentId").value(1042));
+    }
+
+    
 }
