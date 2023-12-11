ed049726c665fe3d6fac10681d56c8bfba46e583
Tim <persontim11@gmail.com>
2023-12-10 21:49:34 -0500

    NoFilters_FullTest added

--- sdm-iso-back/src/test/java/com/example/sdmisoback/UnitTests/FiltersRepoTest.java
+++ sdm-iso-back/src/test/java/com/example/sdmisoback/UnitTests/FiltersRepoTest.java
@@ -19,15 +19,11 @@ import com.example.sdmisoback.dto.FileViewDTO;
 import com.example.sdmisoback.dto.FiltersDTO;
 import com.example.sdmisoback.repository.FiltersRepo;
 
-
-
 @RunWith(SpringRunner.class)
-@SpringBootTest(    
-    webEnvironment = WebEnvironment.MOCK,
-    classes = BackendServerApp.class)
+@SpringBootTest(webEnvironment = WebEnvironment.MOCK, classes = BackendServerApp.class)
 @TestPropertySource(locations = "classpath:test.properties")
 public class FiltersRepoTest {
-    
+
     @Autowired
     FiltersRepo filtersRepo;
 
@@ -41,10 +37,11 @@ public class FiltersRepoTest {
 
     /**
      * Sanity Check that the first page actually behaves like a first page
-     * @param r                 Page result from the Repo call
-     * @throws AssertionError   If any of the tests fail
+     * 
+     * @param r Page result from the Repo call
+     * @throws AssertionError If any of the tests fail
      */
-    private void runFirstPageTests(Page<FileViewDTO> r) throws AssertionError{
+    private void runFirstPageTests(Page<FileViewDTO> r) throws AssertionError {
         Assertions.assertTrue(r.getNumber() == 0, "Not on Page 0");
         Assertions.assertFalse(r.hasPrevious(), "First Page has Previous");
     }
@@ -52,13 +49,15 @@ public class FiltersRepoTest {
     /**
      * Tests to make sure that Page Size behavior is acting as expected and that
      * the content returned also follows those rules
-     * @param r                 Page result from the repository call
-     * @param ExpectedPageSize  Page size that the test expects to be returned
-     * @throws AssertionError   If any of the tests fail
+     * 
+     * @param r                Page result from the repository call
+     * @param ExpectedPageSize Page size that the test expects to be returned
+     * @throws AssertionError If any of the tests fail
      */
-    private void runNotLastPageTests(Page<FileViewDTO> r, int ExpectedPageSize) throws AssertionError{
+    private void runNotLastPageTests(Page<FileViewDTO> r, int ExpectedPageSize) throws AssertionError {
         Assertions.assertTrue(r.getSize() == ExpectedPageSize, "Not Page Size " + ExpectedPageSize);
-        Assertions.assertTrue(r.getNumberOfElements() == ExpectedPageSize, "Page elements don't add up to " + ExpectedPageSize);
+        Assertions.assertTrue(r.getNumberOfElements() == ExpectedPageSize,
+                "Page elements don't add up to " + ExpectedPageSize);
         Assertions.assertFalse(r.getContent().isEmpty(), "Content is empty");
         Assertions.assertTrue(r.getContent().size() == ExpectedPageSize, "Content is not of size " + ExpectedPageSize);
     }
@@ -66,41 +65,89 @@ public class FiltersRepoTest {
     @Test
     public void FiltersRepo_NoFilters_FirstPageIsValid() {
         FiltersDTO filters = FiltersDTO.builder()
-            .pr(defaultPageRequest)
-            .sortBy(sortByCreateDate)
-            .sortAsc(true)
-            .build();
-        
+                .pr(defaultPageRequest)
+                .sortBy(sortByCreateDate)
+                .sortAsc(true)
+                .build();
+
         Page<FileViewDTO> r = filtersRepo.filterAttachments(filters);
         List<FileViewDTO> c = r.getContent();
-        
+
         Assertions.assertTrue(r.isFirst(), "Page that is first is not first");
-        if(r.isFirst())
+        if (r.isFirst())
             runFirstPageTests(r);
-            
-        Assertions.assertFalse(r.isLast(), "Page that is not last is last");      
-        if(!r.isLast())
+
+        Assertions.assertFalse(r.isLast(), "Page that is not last is last");
+        if (!r.isLast())
             runNotLastPageTests(r, 10);
 
         Assertions.assertFalse(r.isEmpty(), "Page is empty when it shouldn't be");
-        Assertions.assertTrue(r.getTotalPages() == Math.ceil(TOTAL_NUM_ELEMENTS / 10), 
-            "Total Pages expected: " + (Math.ceil(TOTAL_NUM_ELEMENTS / 10) + " but was: " + r.getTotalPages()));
+        Assertions.assertTrue(r.getTotalPages() == Math.ceil(TOTAL_NUM_ELEMENTS / 10),
+                "Total Pages expected: " + (Math.ceil(TOTAL_NUM_ELEMENTS / 10) + " but was: " + r.getTotalPages()));
         Assertions.assertTrue(r.getTotalElements() == TOTAL_NUM_ELEMENTS, "Total num search results is off");
 
         // Sort test
         boolean firstPageSorted = true;
         LocalDateTime prevFileDate = null;
-        for(FileViewDTO file : c){
-            if(prevFileDate == null){
+        for (FileViewDTO file : c) {
+            if (prevFileDate == null) {
                 prevFileDate = file.fileCreateDate;
                 continue;
             }
-            if(file.fileCreateDate.compareTo(prevFileDate) < 0){
+            if (file.fileCreateDate.compareTo(prevFileDate) < 0) {
                 firstPageSorted = false;
                 break;
             }
             prevFileDate = file.fileCreateDate;
         }
         Assertions.assertTrue(firstPageSorted, "first page is not sorted correctly");
+    }
+
+    @Test
+    public void FiltersRepo_NoFilters_FullTest() {
+        boolean isLast = false;
+        int pageNum = 0;
+        LocalDateTime prevFileDate = null;
+        while (!isLast) {
+            FiltersDTO filters = FiltersDTO.builder()
+                    .pr(PageRequest.of(pageNum, 10))
+                    .sortBy(sortByCreateDate)
+                    .sortAsc(true)
+                    .build();
+
+            Page<FileViewDTO> r = filtersRepo.filterAttachments(filters);
+            List<FileViewDTO> c = r.getContent();
+
+            if (r.isFirst()) {
+                runFirstPageTests(r);
+                Assertions.assertTrue(r.getTotalPages() == Math.ceil(TOTAL_NUM_ELEMENTS / 10),
+                        "Total Pages expected: "
+                                + (Math.ceil(TOTAL_NUM_ELEMENTS / 10) + " but was: " + r.getTotalPages()));
+                Assertions.assertTrue(r.getTotalElements() == TOTAL_NUM_ELEMENTS, "Total num search results is off");
+            }
+
+            if (!r.isLast()) {
+                runNotLastPageTests(r, 10);
+            } else {
+                isLast = true;
+            }
+
+            Assertions.assertFalse(r.isEmpty(), "Page is empty when it shouldn't be");
+
+            boolean firstPageSorted = true;
+            for (FileViewDTO file : c) {
+                if (prevFileDate == null) {
+                    prevFileDate = file.fileCreateDate;
+                    continue;
+                }
+                if (file.fileCreateDate.compareTo(prevFileDate) < 0) {
+                    firstPageSorted = false;
+                    break;
+                }
+                prevFileDate = file.fileCreateDate;
+            }
+            Assertions.assertTrue(firstPageSorted, "Pages not Sorted correctly");
+            pageNum++;
+        }
     }
 }
